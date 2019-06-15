6a7813e4a2dac13b41bfc40ee7fa884034b74e7f
EduhDev <eduardocarvalho970@gmail.com>
2019-06-15 17:09:22 -0300

    function post concluded

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,10 +1,194 @@
 import React, { Component } from 'react';
 
-import { View } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text, TextInput, Image, KeyboardAvoidingView } from 'react-native';
+
+import ImagePicker from 'react-native-image-picker';
+
+import api from '../services/api';
 
 
 export default class New extends Component {
+  static navigationOptions = {
+    headerTitle: "Novo Post"
+  };
+
+  state = {
+    preview: null,
+    image: null,
+    author: "",
+    place: "",
+    description: "",
+    hashtags: ""
+  }
+
+  handleSelectImage = () => {
+    ImagePicker.showImagePicker({
+      title: 'Selecionar Imagem',
+    }, upload => {
+      if (upload.error) {
+        console.log('Error');
+      } else if (upload.didCancel) {
+        console.log("User canceled");
+      } else {
+        const preview = {
+          uri: `data:image/jpeg;base64,${upload.data}`
+        }
+
+        let prefix;
+        let ext;
+
+        if(upload.fileName) {
+          [prefix, ext] = upload.fileName.split('.');
+          ext = ext.toLocaleLowerCase() === 'heic' ? 'jpg' : ext;
+        } else {
+          prefix= new Date().getTime();
+          ext= 'jpg';
+        }
+
+        const image = {
+          uri: upload.uri,
+          type: upload.type,
+          name: `${prefix}.${ext}`
+        };
+
+        this.setState({image})
+        this.setState({preview})
+      }
+    })
+  }
+
+  handleSubmit = async () => {
+    const data = new FormData();
+
+    data.append('image', this.state.image);
+    data.append('description', this.state.description);
+    data.append('author', this.state.author);
+    data.append('place', this.state.place);
+    data.append('hashtags', this.state.hashtags);
+
+    await api.post('posts', data);
+
+    this.props.navigation.navigate('Feed');
+    this.setState({
+      image: null,
+      preview: null,
+      author: "",
+      place: "",
+      description: "",
+      hashtags: ""
+    })
+  }
+
   render() {
-    return <View />;
+    return (
+      <KeyboardAvoidingView style={styles.container}>
+        <TouchableOpacity style={styles.selectButton} onPress={this.handleSelectImage}>
+          <Text style={styles.selectButtonText}>Selecionar Image</Text>
+        </TouchableOpacity>
+
+        {this.state.preview && <Image style={styles.preview} source={this.state.preview} />}
+        <TextInput
+          style={styles.input}
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Nome do Autor"
+          placeholderTextColor="#999"
+          value={this.state.author}
+          onChangeText={author => this.setState({ author })}
+        />
+
+        <TextInput
+          style={styles.input}
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Lugar da Postagem"
+          placeholderTextColor="#999"
+          value={this.state.place}
+          onChangeText={place => this.setState({ place })}
+        />
+
+        <TextInput
+          style={styles.input}
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Descricao da Postagem"
+          placeholderTextColor="#999"
+          value={this.state.description}
+          onChangeText={description => this.setState({ description })}
+        />
+
+        <TextInput
+          style={styles.input}
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Tags da Postagem"
+          placeholderTextColor="#999"
+          value={this.state.hashtags}
+          onChangeText={hashtags => this.setState({ hashtags })}
+        />
+
+        <TouchableOpacity style={styles.shareButton} onPress={this.handleSubmit}>
+          <Text style={styles.shareButtonText}>Compartilhar</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    )
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingTop: 30,
+  },
+
+  selectButton: {
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: '#CCC',
+    borderStyle: 'dashed',
+    height: 42,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  selectButtonText: {
+    fontSize: 16,
+    color: '#666',
+  },
+
+  preview: {
+    width: 300,
+    height: 300,
+    marginTop: 10,
+    alignSelf: 'center',
+    borderRadius: 4,
+  },
+
+  input: {
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 15,
+    marginTop: 10,
+    fontSize: 16,
+  },
+
+  shareButton: {
+    backgroundColor: '#7159c1',
+    borderRadius: 4,
+    height: 42,
+    marginTop: 15,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  shareButtonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#FFF',
+  },
+});
